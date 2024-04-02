bc67615fc8c1c837d5a656b2ebec233125114db0
sharks22dm <165840551+sharks22dm@users.noreply.github.com>
2024-04-02 23:30:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import modules_and_packages as mod1
+
+mod1.function_1()
+mod1.function_2()
---
+++ modules_and_packages.py
@@ -0,0 +1,7 @@
+def function_1():
+    print('Hello, world')
+
+
+def function_2():
+    print('Hello, world')
+    
